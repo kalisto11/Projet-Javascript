05deda1b1151d2cc9ec08dd94eb37982217cc614
Khadim Diaw <khadim.diaw@gmail.com>
2021-04-28 14:35:11 +0000

    save php

--- senmoney.php
+++ senmoney.php
@@ -3,7 +3,7 @@
     $retour = $pdo->query('SELECT numero FROM comptes');
     $comptes = array();
     while ($compte = $retour->fetch()){
-        $comptes[] =  $compte;
+        $comptes[] = $compte;
     }
     exit(json_encode($comptes));
 ?>
